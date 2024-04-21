034f4a01ec198e58f1085bfc1fc68fbd42490782
Xuan Lam <chim31102007@gmail.com>
2024-04-21 21:38:31 +0700

    Initial commit

---
+++ src/main/java/me/hoangxuanlam2007/chuyenexp/ChuyenExp.kt
@@ -0,0 +1,13 @@
+package me.hoangxuanlam2007.chuyenexp
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ChuyenExp : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
